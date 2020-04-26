96b6c54386a474873b442ee070eb6c4f2a318341
khai96_ <hvksmr1996@gmail.com>
2020-04-26 18:50:47 +0700

    Add --write

--- src/cli_opt/mod.rs
+++ src/cli_opt/mod.rs
@@ -6,6 +6,10 @@ pub use detail_level::*;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "sane-fmt", rename_all = "kebab")]
 pub struct CliOpt {
+  /// Whether to write or check
+  #[structopt(long, short = "w")]
+  write: bool,
+
   /// File diff detail
   /// [possible values: count, name, diff]
   #[structopt(long, default_value = "diff")]
