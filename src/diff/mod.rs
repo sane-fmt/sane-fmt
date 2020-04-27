4d8fe1bdb0b4967d26e640852a325c1ff3f935b4
khai96_ <hvksmr1996@gmail.com>
2020-04-27 20:53:25 +0700

    fmt

--- src/diff/mod.rs
+++ src/diff/mod.rs
@@ -1,18 +1,16 @@
-use difference::Difference::*;
 pub use difference::Changeset as Diff;
+use difference::Difference::*;
 
 pub fn diff(old: &String, new: &String) -> Diff {
     Diff::new(old.as_str(), new.as_str(), "\n")
 }
 
 pub fn diff_lines(old: &String, new: &String) -> impl Iterator<Item = String> {
-    diff(old, new).diffs
-        .into_iter()
-        .map(|diff| match diff {
-            Same(line) => add_prefix(line, "   "),
-            Add(line) => add_prefix(line, "  +"),
-            Rem(line) => add_prefix(line, "  -"),
-        })
+    diff(old, new).diffs.into_iter().map(|diff| match diff {
+        Same(line) => add_prefix(line, "   "),
+        Add(line) => add_prefix(line, "  +"),
+        Rem(line) => add_prefix(line, "  -"),
+    })
 }
 
 fn add_prefix(text: String, prefix: &str) -> String {
