d85850f74b460cf26e4b0c6defba381ceeaa7274
khai96_ <hvksmr1996@gmail.com>
2020-04-28 17:44:28 +0700

    Remove a mod

--- src/term/color.rs
+++ src/term/color.rs
@@ -10,30 +10,27 @@ pub struct ColorScheme<C: Color> {
     pub find_name: C,
 }
 
-pub mod collection {
-    use super::Color;
-    pub use ansi_term::Style;
+pub use ansi_term::Style;
 
-    pub struct Colorless;
-    impl Color for Colorless {
-        fn paint(&self, text: &str) -> String {
-            text.to_owned()
-        }
+pub struct Colorless;
+impl Color for Colorless {
+    fn paint(&self, text: &str) -> String {
+        text.to_owned()
     }
+}
 
-    pub struct Colorful {
-        style: Style,
+pub struct Colorful {
+    style: Style,
+}
+
+impl Colorful {
+    pub fn new(style: Style) -> Self {
+        Colorful { style }
     }
+}
 
-    impl Colorful {
-        pub fn new(style: Style) -> Self {
-            Colorful { style }
-        }
-    }
-
-    impl Color for Colorful {
-        fn paint(&self, text: &str) -> String {
-            format!("{}", self.style.paint(text))
-        }
+impl Color for Colorful {
+    fn paint(&self, text: &str) -> String {
+        format!("{}", self.style.paint(text))
     }
 }
