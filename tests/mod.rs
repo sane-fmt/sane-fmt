164b93564167ca91248d1853bbe8b26f625cebf6
khai96_ <hvksmr1996@gmail.com>
2020-04-29 17:26:42 +0700

    Create color_match_non_color

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,3 +1,30 @@
 #![cfg(test)]
 pub mod utils;
 pub use utils::*;
+
+use strip_ansi_escapes::strip as strip_ansi;
+
+#[test]
+fn color_match_non_color() {
+    let with_color = Exe::workspace()
+        .mut_cmd(|cmd| {
+            cmd.arg("--show-skipped")
+                .arg("--details=diff")
+                .arg("--color=always");
+        })
+        .cmd
+        .output()
+        .expect("spawn command with color")
+        .stdout;
+    let without_color = Exe::workspace()
+        .mut_cmd(|cmd| {
+            cmd.arg("--show-skipped")
+                .arg("--details=diff")
+                .arg("--color=never");
+        })
+        .cmd
+        .output()
+        .expect("spawn command without color")
+        .stdout;
+    debug_assert_eq!(strip_ansi(with_color).unwrap(), without_color);
+}
