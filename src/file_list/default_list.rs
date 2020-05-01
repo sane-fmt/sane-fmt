eb2f12cb45ca6585de2996c153bf2ba3e7d766d6
khai96_ <hvksmr1996@gmail.com>
2020-05-01 16:11:11 +0700

    Ensure deterministic order

--- src/file_list/default_list.rs
+++ src/file_list/default_list.rs
@@ -9,10 +9,17 @@ pub const IGNORED_NAMES: &[&str] = &[".git", "node_modules"];
 pub const EXTENSIONS: &[&str] = &["ts", "tsx", "js", "jsx"];
 
 fn add_files(list: &mut List, dirname: &PathBuf) -> io::Result<()> {
-    for res in read_dir(dirname)? {
-        let entry = res?;
-        let path = entry.path();
-        let file_type = symlink_metadata(&path)?.file_type();
+    let mut entries = read_dir(dirname)?
+        .map(|entry| -> io::Result<_> {
+            let entry = entry?;
+            let path = entry.path();
+            let metadata = symlink_metadata(&path)?;
+            Ok((entry, path, metadata))
+        })
+        .collect::<io::Result<Vec<_>>>()?;
+    entries.sort_by(|(_, a, _), (_, b, _)| a.cmp(b));
+    for (entry, path, metadata) in entries {
+        let file_type = metadata.file_type();
         if file_type.is_dir() {
             let name = entry.file_name().to_string_lossy().to_string();
             if !IGNORED_NAMES.contains(&name.as_str()) {
