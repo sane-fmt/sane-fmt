870ca2980531eaa6bbbf6a0d51db67888208a683
khai96_ <hvksmr1996@gmail.com>
2020-05-03 20:14:29 +0700

    Advance in progress

--- nodejs/wasm32-wasi/index.ts
+++ nodejs/wasm32-wasi/index.ts
@@ -1,3 +1,52 @@
+import path from 'path'
+import fs from 'fs'
 import { WASI } from '@wasmer/wasi'
-import wasiBindings from '@wasmer/wasi/lib/bindings/node'
+import bindings from '@wasmer/wasi/lib/bindings/node'
 import { WasmFs } from '@wasmer/wasmfs'
+
+export const wasmFile = require.resolve('./sane-fmt.wasm')
+
+export type WriteStream = NodeJS.WriteStream
+
+export interface Process {
+  readonly argv: readonly string[]
+  readonly cwd: () => string
+  readonly stdout: WriteStream
+  readonly stderr: WriteStream
+  readonly exit: (status: number) => never
+}
+
+export async function main(process: Process): Promise<void> {
+  const args = process.argv.slice(2)
+  const wdir = process.cwd()
+  const wasmFs = new WasmFs()
+
+  const wasi = new WASI({
+    args,
+    preopenDirectories: {
+      '/': path.parse(wdir).root,
+      '.': wdir,
+    },
+    bindings: {
+      ...bindings,
+      fs: wasmFs,
+      exit: status => process.exit(status)
+    },
+  })
+
+  function pipeFile (path: string, stream: WriteStream) {
+    const reader = wasmFs.fs.createReadStream(path)
+    reader.on('open', () => reader.pipe(stream))
+  }
+
+  pipeFile('/dev/stdout', process.stdout)
+  pipeFile('/dev/stderr', process.stderr)
+
+  const wasmBytes = fs.readFileSync(wasmFile)
+  const wasmModule = await WebAssembly.compile(wasmBytes)
+  const wasmInstance = await WebAssembly.instantiate(wasmModule, {
+    ...wasi.getImports(wasmModule)
+  })
+
+  wasi.start(wasmInstance)
+}
