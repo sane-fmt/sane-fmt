c5dee5ba3852642dcc9a65345dd16acd2dc1082c
khai96_ <hvksmr1996@gmail.com>
2020-05-03 12:51:43 +0700

    Complete expose-release-artifacts.sh

---
+++ ci/github-actions/expose-release-artifacts.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+mkdir ./flatten
+
+[ -d ./downloads ] || {
+  echo Folder ./downloads does not exist > /dev/stderr
+  exit 1
+}
+
+for folder in ./downloads/*; do
+  echo Copying "$folder"/* ...
+  cp "$folder"/* ./flatten/ || exit $?
+done
