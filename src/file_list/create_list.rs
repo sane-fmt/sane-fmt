32aef2fa9ad7b80acefb9f98b649e270be6ef2cd
khai96_ <hvksmr1996@gmail.com>
2020-05-03 10:49:47 +0700

    Convert directory names to lists of files

--- src/file_list/create_list.rs
+++ src/file_list/create_list.rs
@@ -1,4 +1,4 @@
-use super::{Item, List};
+use super::{read_dir::read_into, Item, List};
 use std::{fs::metadata, io, path::PathBuf};
 
 pub fn create_list<Input>(files: Input) -> io::Result<List>
@@ -8,6 +8,12 @@ where
     let mut result = List::new();
 
     for path in files {
+        let file_type = metadata(&path)?.file_type();
+
+        if file_type.is_dir() {
+            read_into(&mut result, &path)?;
+        }
+
         result.push(Item {
             file_type: metadata(&path)?.file_type(),
             path,
