9483b583eb067d19e3c102befdb90603b9b91d39
khai96_ <hvksmr1996@gmail.com>
2020-05-04 09:20:04 +0700

    Add PNPM to deploy.yaml

---
+++ ci/install-pnpm.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/sh
+
+if [ -z "$PNPM_VERSION" ]; then
+  # shellcheck disable=SC2016
+  echo '[ERROR] $PNPM_VERSION is undefined' > /dev/stderr
+  exit 1
+fi
+
+ref=301414cec74a2b6b63c95b42f2ad1790ccb980ed
+exec curl -fsSL https://raw.githubusercontent.com/pnpm/self-installer/$ref/install.js | node
