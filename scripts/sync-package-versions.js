01e3803aac6277f7964f176cfabedb3f925a98e4
khai96_ <hvksmr1996@gmail.com>
2020-05-04 18:26:27 +0700

    Create script to keep versions in sync

---
+++ scripts/sync-package-versions.js
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+const console = require('console')
+const fs = require('fs')
+const process = require('process')
+const toml = require('toml')
+
+const cargoManifest = require.resolve('../Cargo.toml')
+const wasmManifest = require.resolve('../nodejs/wasm32-wasi/package.json')
+
+const version = toml.parse(fs.readFileSync(cargoManifest)).package?.version
+const wasmData = require(wasmManifest)
+
+if (typeof version !== 'string') {
+  console.error(`Expecting Cargo.toml#package.version to be a string but received a ${typeof version} instead`)
+  throw process.exit(1)
+}
+
+if (version !== wasmData.version) {
+  console.info(`Version mismatch: ${version} vs ${wasmData.version}. Correcting...`)
+  wasmData.version = version
+  const json = JSON.stringify(wasmData, undefined, 2) + '\n'
+  fs.writeFileSync(wasmManifest, json)
+}
