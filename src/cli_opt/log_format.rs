49d8684bfeef31357e72fad04cb74a79dd637cb7
khai96_ <hvksmr1996@gmail.com>
2020-05-12 17:47:45 +0700

    Add --log-format

---
+++ src/cli_opt/log_format.rs
@@ -0,0 +1,17 @@
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum LogFormat {
+    Human,
+    GitHubActions,
+}
+
+impl std::str::FromStr for LogFormat {
+    type Err = String;
+
+    fn from_str(text: &str) -> Result<Self, Self::Err> {
+        Ok(match text {
+            "human" => LogFormat::Human,
+            "github-actions" => LogFormat::GitHubActions,
+            _ => return Err(text.to_owned()),
+        })
+    }
+}
