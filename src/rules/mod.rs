960400d053bbe8905322b907a08090e43c6209b5
khai96_ <hvksmr1996@gmail.com>
2020-05-26 23:00:48 +0700

    Use pipe method

--- src/rules/mod.rs
+++ src/rules/mod.rs
@@ -5,6 +5,7 @@ pub use dprint_plugin_typescript::{
     configuration::{Configuration as Cfg, ConfigurationBuilder as CfgBuilder},
     Formatter as Fmt,
 };
+use pipe_trait::*;
 
 /// Shape a `ConfigurationBuilder` to desired configuration.
 pub fn modify(builder: &mut CfgBuilder) -> &mut CfgBuilder {
@@ -18,9 +19,7 @@ pub fn modify(builder: &mut CfgBuilder) -> &mut CfgBuilder {
 
 /// Create desired configuration.
 pub fn build_cfg() -> Cfg {
-    let mut builder = CfgBuilder::new();
-    modify(&mut builder);
-    builder.build()
+    CfgBuilder::new().pipe_mut(modify).build()
 }
 
 /// Create a formatter for desired configuration.
