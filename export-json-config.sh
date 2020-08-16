9c87bf6c5524c762bd4173cde39a8d4ae01e7eb7
khai96_ <hvksmr1996@gmail.com>
2020-08-16 09:52:49 +0700

    Consistent filenames

--- export-json-config.sh
+++ export-json-config.sh
@@ -3,5 +3,5 @@ set -o errexit pipefail nounset
 
 cd "$(dirname "$0")"
 mkdir -pv exports
-cargo run --bin=sane-fmt-json typescript --output=exports/sane-fmt-typescript.json
+cargo run --bin=sane-fmt-json typescript --output=exports/sane-fmt.typescript.json
 cargo run --bin=sane-fmt-json dprintrc --output=exports/sane-fmt.dprintrc.json
