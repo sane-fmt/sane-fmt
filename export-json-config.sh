a008fac0265253f8d19bd8f9e2e047ef20927d0f
khai96_ <hvksmr1996@gmail.com>
2020-08-16 10:00:42 +0700

    refactor: Create 'run' function

--- export-json-config.sh
+++ export-json-config.sh
@@ -3,5 +3,10 @@ set -o errexit pipefail nounset
 
 cd "$(dirname "$0")"
 mkdir -pv exports
-cargo run --bin=sane-fmt-export-json-config typescript --output=exports/sane-fmt.typescript.json
-cargo run --bin=sane-fmt-export-json-config dprintrc --output=exports/sane-fmt.dprintrc.json
+
+run() {
+  cargo run --bin=sane-fmt-export-json-config -- "$1" --output="exports/sane-fmt.$1.json"
+}
+
+run typescript
+run dprintrc
