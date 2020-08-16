6f75a2f0b5af66b87b22a48e2b89565fc31f1efa
khai96_ <hvksmr1996@gmail.com>
2020-08-16 09:48:31 +0700

    Create script to export JSON files

---
+++ export-json-config.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+set -o errexit pipefail nounset
+
+cd "$(dirname "$0")"
+mkdir -pv exports
+cargo run --bin=sane-fmt-json typescript --output=exports/sane-fmt-typescript.json
+cargo run --bin=sane-fmt-json dprint --output=exports/sane-fmt.dprintrc.json
