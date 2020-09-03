212e9e1c015935b9d2c9f06109def50dbf72dac0
khai96_ <hvksmr1996@gmail.com>
2020-09-03 08:01:25 +0700

    Use `case in`

--- fmt.sh
+++ fmt.sh
@@ -5,16 +5,20 @@ FMT_UPDATE="${FMT_UPDATE:-false}"
 cargo_fmt_flag=()
 sane_fmt_flag=()
 
-if [ "$FMT_UPDATE" = 'true' ]; then
-  cargo_fmt_flag=()
-  sane_fmt_flag=('--write')
-elif [ "$FMT_UPDATE" = 'false' ]; then
-  cargo_fmt_flag=('--check')
-  sane_fmt_flag=()
-else
-  echo "error: \$FMT_UPDATE is neither 'true' or 'false': '$FMT_UPDATE'" > /dev/stderr
-  exit 1
-fi
+case "$FMT_UPDATE" in
+  true)
+    cargo_fmt_flag=()
+    sane_fmt_flag=('--write')
+  ;;
+  false)
+    cargo_fmt_flag=('--check')
+    sane_fmt_flag=()
+  ;;
+  *)
+    echo "error: \$FMT_UPDATE is neither 'true' or 'false': '$FMT_UPDATE'" > /dev/stderr
+    exit 1
+  ;;
+esac
 
 cargo run --bin=sane-fmt nodejs/*/src scripts preview ci "${sane_fmt_flag[@]}"
 cargo fmt -- "${cargo_fmt_flag[@]}"
