30a1cb43fa1e53a7e2999cc5e1f96b96d2b7f7f4
khai96_ <hvksmr1996@gmail.com>
2020-09-05 12:18:00 +0700

    Add doccomment to create_list

--- src/file_list/create_list.rs
+++ src/file_list/create_list.rs
@@ -1,6 +1,10 @@
 use super::{read_dir::read_into, Item, List};
 use std::{fs::metadata, io, path::PathBuf};
 
+/// Create a list of TypeScript/JavaScript files
+///
+/// * Convert directory path to a list of TypeScript/JavaScript files
+/// * Keep file path as-is
 pub fn create_list<Input>(files: Input) -> io::Result<List>
 where
     Input: Iterator<Item = PathBuf>,
