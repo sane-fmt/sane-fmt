0c5d91af15ef20d01f5baa61f4166c22057c59d9
khai96_ <hvksmr1996@gmail.com>
2020-09-05 08:18:45 +0700

    Create tests for --include feature

---
+++ tests/include.rs
@@ -0,0 +1,104 @@
+#![cfg(test)]
+pub mod utils;
+pub use utils::*;
+
+#[test]
+fn stdin_files() {
+    let output = Exe::fixtures().run_with_stdio(
+        include_bytes!("./assets/include-files.input.txt"),
+        &["-I", "-", "--color=never"],
+    );
+    assert_eq!(
+        (
+            u8v_to_utf8(&output.stdout),
+            u8v_to_utf8(&output.stderr),
+            output.status.success(),
+        ),
+        (
+            include_str!("./expected-output/include-files.stdout.txt"),
+            "Error: \"There are 1 unformatted files\"\n",
+            false,
+        ),
+    );
+}
+
+#[test]
+fn stdin_dirs() {
+    let output = Exe::fixtures().run_with_stdio(
+        include_bytes!("./assets/include-dirs.input.txt"),
+        &["-I", "-", "--color=never"],
+    );
+    assert_eq!(
+        (
+            u8v_to_utf8(&output.stdout),
+            u8v_to_utf8(&output.stderr),
+            output.status.success()
+        ),
+        (
+            include_str!("./expected-output/include-dirs.stdout.txt"),
+            "Error: \"There are 5 unformatted files\"\n",
+            false,
+        ),
+    );
+}
+
+#[test]
+fn file_files() {
+    let output = Exe::fixtures()
+        .cmd
+        .arg("--color=never")
+        .arg("--include=../assets/include-files.input.txt")
+        .output()
+        .unwrap();
+    assert_eq!(
+        (
+            u8v_to_utf8(&output.stdout),
+            u8v_to_utf8(&output.stderr),
+            output.status.success(),
+        ),
+        (
+            include_str!("./expected-output/include-files.stdout.txt"),
+            "Error: \"There are 1 unformatted files\"\n",
+            false,
+        ),
+    );
+}
+
+#[test]
+fn file_dirs() {
+    let output = Exe::fixtures()
+        .cmd
+        .arg("--color=never")
+        .arg("--include=../assets/include-dirs.input.txt")
+        .output()
+        .unwrap();
+    assert_eq!(
+        (
+            u8v_to_utf8(&output.stdout),
+            u8v_to_utf8(&output.stderr),
+            output.status.success(),
+        ),
+        (
+            include_str!("./expected-output/include-dirs.stdout.txt"),
+            "Error: \"There are 5 unformatted files\"\n",
+            false,
+        ),
+    );
+}
+
+#[test]
+fn mixed() {
+    let output = Exe::fixtures().run_with_stdio(
+        include_bytes!("./assets/include-mixed.stdin.txt"),
+        &[
+            "--color=never",
+            "--include=-",
+            "--include=../assets/include-mixed.file.txt",
+            "correct/react.tsx",
+        ],
+    );
+    assert_eq!(
+        u8v_to_utf8(&output.stdout),
+        include_str!("./expected-output/include-mixed.stdout.txt"),
+    );
+}
