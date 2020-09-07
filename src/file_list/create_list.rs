dbeaa73e457b23e615d9c3dc47d5b07070d1429b
khai96_ <hvksmr1996@gmail.com>
2020-09-07 10:21:17 +0700

    Reduce function call duplication

--- src/file_list/create_list.rs
+++ src/file_list/create_list.rs
@@ -17,10 +17,7 @@ where
         if file_type.is_dir() {
             read_into(&mut result, &path)?;
         } else {
-            result.push(Item {
-                file_type: metadata(&path)?.file_type(),
-                path,
-            });
+            result.push(Item { file_type, path });
         }
     }
 
