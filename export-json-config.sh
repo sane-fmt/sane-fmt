16172db55ba1a0f60a3ca08b1f818f22a0d4491a
khai96_ <hvksmr1996@gmail.com>
2020-09-28 08:27:09 +0700

    Create run.sh

--- export-json-config.sh
+++ export-json-config.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 mkdir -pv exports
 
 run() {
-	cargo run --bin=sane-fmt-export-json-config -- "$1" --output="exports/sane-fmt.$1.json"
+	./run.sh sane-fmt-export-json-config "$1" --output="exports/sane-fmt.$1.json"
 }
 
 run typescript
