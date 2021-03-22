488fc2fa711fb1d469063bd2c035242c31cac59e
khai96_ <hvksmr1996@gmail.com>
2021-03-22 22:03:29 +0700

    refactor: Only re-export items that are used

--- src/rules/cfg.rs
+++ src/rules/cfg.rs
@@ -1,2 +1,6 @@
-pub use dprint_core::configuration::*;
-pub use dprint_plugin_typescript::configuration::*;
+pub use dprint_core::configuration::NewLineKind;
+pub use dprint_plugin_typescript::configuration::{
+    BracePosition, Configuration, ConfigurationBuilder, MemberSpacing, NextControlFlowPosition,
+    OperatorPosition, QuoteStyle, SemiColonOrComma, SemiColons, SingleBodyPosition, SortOrder,
+    TrailingCommas, UseBraces, UseParentheses,
+};
