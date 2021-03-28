eeb11a2389986f861fb323867b33c5fae10a8ce4
khai96_ <hvksmr1996@gmail.com>
2021-03-28 18:22:20 +0700

    refactor: Create macro test_version!
    making convenient to add other similar tests

--- tests/package_manifests.rs
+++ tests/package_manifests.rs
@@ -5,9 +5,15 @@ pub use utils::*;
 use cargo_toml::*;
 use package_json::*;
 
-#[test]
-fn wasm32_wasi_version() {
-    let native = CargoManifest::load();
-    let nodejs = NodeManifest::parse(include_str!("../nodejs/wasm32-wasi/package.json"));
-    assert_eq!(nodejs.version, native.package.version);
+macro_rules! test_version {
+    ($test_name:ident, $manifest_path:literal) => {
+        #[test]
+        fn $test_name() {
+            let native = CargoManifest::load();
+            let nodejs = NodeManifest::parse(include_str!($manifest_path));
+            assert_eq!(nodejs.version, native.package.version);
+        }
+    };
 }
+
+test_version!(wasm32_wasi_version, "../nodejs/wasm32-wasi/package.json");
