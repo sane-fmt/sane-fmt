fec0036821f3914fa1d7c29cd4b4b24f2ec5feaa
khai96_ <hvksmr1996@gmail.com>
2022-10-15 15:41:08 +0700

    test: GitHub Actions format with GITHUB_OUTPUT

---
+++ tests/github_actions.rs
@@ -0,0 +1,105 @@
+#![cfg(test)]
+pub mod utils;
+
+pub use utils::*;
+
+use ansi_term::*;
+use std::fs::read_to_string;
+
+fn gh_output_file() -> tempfile::NamedTempFile {
+    tempfile::Builder::new()
+        .prefix("github-output")
+        .suffix(".txt")
+        .tempfile()
+        .expect("create temporary file for GITHUB_OUTPUT")
+}
+
+#[test]
+fn details_diff() {
+    let gh_output_file = gh_output_file();
+
+    let output = Exe::fixtures()
+        .cmd
+        .arg("--log-format=github-actions")
+        .arg("--details=diff")
+        .arg("--color=never")
+        .env("GITHUB_OUTPUT", gh_output_file.path())
+        .output()
+        .unwrap();
+
+    assert_trimmed_str_eq(
+        visualize_command_output(&output, &Style::new()).as_str(),
+        visualize_fake_command_output(
+            1,
+            include_str!("./expected-output/github-actions/details-diff.stdout.txt"),
+            include_str!("./expected-output/stderr.txt"),
+            &Style::new(),
+        )
+        .as_str(),
+    );
+
+    assert_trimmed_str_eq(
+        &read_to_string(gh_output_file.path()).expect("read GITHUB_OUTPUT file"),
+        include_str!("./expected-output/github-actions/github-output.txt"),
+    );
+}
+
+#[test]
+fn details_name() {
+    let gh_output_file = gh_output_file();
+
+    let output = Exe::fixtures()
+        .cmd
+        .arg("--log-format=github-actions")
+        .arg("--details=name")
+        .arg("--color=never")
+        .env("GITHUB_OUTPUT", gh_output_file.path())
+        .output()
+        .unwrap();
+
+    assert_trimmed_str_eq(
+        visualize_command_output(&output, &Style::new()).as_str(),
+        visualize_fake_command_output(
+            1,
+            include_str!("./expected-output/github-actions/details-name.stdout.txt"),
+            include_str!("./expected-output/stderr.txt"),
+            &Style::new(),
+        )
+        .as_str(),
+    );
+
+    assert_trimmed_str_eq(
+        &read_to_string(gh_output_file.path()).expect("read GITHUB_OUTPUT file"),
+        include_str!("./expected-output/github-actions/github-output.txt"),
+    );
+}
+
+#[test]
+fn details_count() {
+    let gh_output_file = gh_output_file();
+
+    let output = Exe::fixtures()
+        .cmd
+        .arg("--log-format=github-actions")
+        .arg("--details=count")
+        .arg("--color=never")
+        .env("GITHUB_OUTPUT", gh_output_file.path())
+        .output()
+        .unwrap();
+
+    assert_trimmed_str_eq(
+        visualize_command_output(&output, &Style::new()).as_str(),
+        visualize_fake_command_output(
+            1,
+            include_str!("./expected-output/github-actions/details-count.stdout.txt"),
+            include_str!("./expected-output/stderr.txt"),
+            &Style::new(),
+        )
+        .as_str(),
+    );
+
+    assert_trimmed_str_eq(
+        &read_to_string(gh_output_file.path()).expect("read GITHUB_OUTPUT file"),
+        include_str!("./expected-output/github-actions/github-output.txt"),
+    );
+}
